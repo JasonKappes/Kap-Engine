117494a52131609cde257f715bcf6ffafa4dd82f
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-07-31 18:34:04 -0500

    Update SpriteFontManager.h
    Added Comments

--- Asset Management/Sprites/SpriteFontManager.h
+++ Asset Management/Sprites/SpriteFontManager.h
@@ -6,34 +6,23 @@
 class KapSprite;
 class SpriteFont;
 
+//manages sprite resources
 class SpriteFontManager
 {
 	friend class SpriteFontManagerAttorney;
-public: // ----> USER METHODS
+public: /// ----> USER METHODS
 
-	/// <summary>
 	/// Loads a 2D sprite/image
-	/// 
 	/// Takes a user-defined sprite name and a user-defined path to load the sprite. To be called from LoadAllResources.cpp
-	/// </summary> 
-	/// \ingroup SpriteFontManager
 	static void Load(const char* t_chosenName , const char* t_fileName) { Instance().privLoad(t_chosenName, t_fileName); }
 
-	/// <summary>
 	/// Retrieves a Sprite-style Font for use by a sprite string.
-	/// 
 	/// Takes a user-defined path to the sprite font as input.
-	/// </summary> 
-	/// \ingroup SpriteFontManager
 	static SpriteFont* Get(const char* t_fileName) { return Instance().privGet(t_fileName); }
 
 public:
-	/// <summary>
 	/// A default sprite font to load is Dosis2. Load this font (to use for a SpriteString) in LoadAllResources.cpp.
-	/// 
 	/// Default Key Syntax: 	SpriteFontManager::Load("Fonts/Dosis2", "Fonts/Dosis2");
-	/// </summary> 
-	/// \ingroup SpriteFontManager
 	static std::string defaultKeyAsset;
 
 private:

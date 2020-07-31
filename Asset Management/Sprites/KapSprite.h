53ef369d535ea9be08c0a5a26576aafcb60b4abe
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-07-31 18:25:37 -0500

    Update KapSprite.h
    Added Comments

--- Asset Management/Sprites/KapSprite.h
+++ Asset Management/Sprites/KapSprite.h
@@ -11,72 +11,39 @@ class Camera;
 
 class KapSprite
 {
-public: // ----> USER METHODS
+public: /// ----> USER METHODS
 
-	/// <summary>
 	/// This method should be called on a KapSprite object from Draw2D() to show this sprite on screen.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void Render();
 
-	/// <summary>
 	/// This method sets the rotation, or angle, of a KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void SetAngle(float angle);
 
-	/// <summary>
 	/// This method sets the pixel center, rotation point, of a KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void SetCenter(float centerX, float centerY);
 
-	/// <summary>
 	/// This method sets the position on screen where the KapSprite object will be rendered.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void SetPosition(float positionX, float positionY);
 
-	/// <summary>
 	/// This method multiplies the size of the KapSprite by scale input given.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void SetScaleFactor(float scalex, float scaley);
 
-	/// <summary>
 	/// This method sets the exact pixel width and height of the KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	void SetScalePixel(float width, float height);
 
-	/// <summary>
 	/// Returns the angle at which the KapSprite is rotated at.
-	/// </summary> 
-	/// \ingroup KapSprite
 	float GetAngle();
 
-	/// <summary>
 	/// Returns the horizontal (x) position of the KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	float GetX();
 
-	/// <summary>
 	/// Returns the vertical (y) position of the KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	float GetY();
 
-	/// <summary>
 	/// Returns the pixel width of the KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	float GetWidth();
 
-	/// <summary>
 	/// Returns the pixel height of the KapSprite.
-	/// </summary> 
-	/// \ingroup KapSprite
 	float GetHeight();
 
 public:
@@ -93,6 +60,11 @@ private:
 	Rect* m_rectPosSize;
 	Model* m_model;
 	Image* m_image;
+	
+	Matrix m_worldScale;
+	Matrix m_rotZ;
+	Matrix m_world;
+	Matrix m_trans;
 
 	float m_rotAngle;
 	float m_centerX;
@@ -101,11 +73,6 @@ private:
 	float m_scaleY;
 	float m_width;
 	float m_height;
-
-	Matrix m_worldScale;
-	Matrix m_rotZ;
-	Matrix m_world;
-	Matrix m_trans;
 
 };
 

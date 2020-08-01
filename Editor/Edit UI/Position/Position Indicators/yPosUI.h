a1248683959038fd6843f2f230a417cb6833254c
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-07-31 19:42:02 -0500

    Update yPosUI.h
    added comments

--- Editor/Edit UI/Position/Position Indicators/yPosUI.h
+++ Editor/Edit UI/Position/Position Indicators/yPosUI.h
@@ -4,6 +4,7 @@
 
 class PositionUI;
 
+//This class controls the behavior for the Y position indicator for a selected object
 class yPosUI : public WorldUI
 {
 public:
@@ -14,12 +15,16 @@ public:
 
 	yPosUI(PositionUI*);
 
+	//This describes the behavior if this indicator is dragged upward
 	void ForwardMovement(Serializable*) override;
 
+	//This describes the behavior if this indicator is dragged downward
 	void BackwardMovement(Serializable*) override;
 
+	//This displays this indicator, when told too
 	void Display(float radius, const Vect& center) override;
 
+	//This is a callback when this indicator is selected
 	void Selected() override;
 
 private:

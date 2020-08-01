f704c62b2f125d050e1317985f9355d1bf71ec47
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-07-31 19:38:43 -0500

    Update xPosUI.h
    Added Comments

--- Editor/Edit UI/Position/Position Indicators/xPosUI.h
+++ Editor/Edit UI/Position/Position Indicators/xPosUI.h
@@ -4,24 +4,28 @@
 
 class PositionUI;
 
+//This class controls the behavior for the X Position Indicator for a selected object
 class xPosUI : public WorldUI
 {
-public:
+public: // ENGINE METHODS
 	xPosUI() = delete;
 	xPosUI(const xPosUI&) = delete;
 	xPosUI operator=(const xPosUI&) = delete;
 	~xPosUI();
 
 	xPosUI(PositionUI*);
-
+	
+	//Displays the position indicator
 	void Display(float radius, const Vect& center) override;
-
+	
+	//A callback if this indicator is selected
 	void Selected() override;
 
+	//The behavior if the indicator is moved left
 	void LeftMovement(Serializable*) override;
 
+	//The behavior if the indicator is moved right
 	void RightMovement(Serializable*) override;
-
 
 private:
 	PositionUI* posUIcoll;

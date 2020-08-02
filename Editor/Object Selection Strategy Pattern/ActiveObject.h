2ae942a50fc09459ab55391d6a89017f76b92edd
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 12:04:38 -0500

    Update ActiveObject.h
    added comments

--- Editor/Object Selection Strategy Pattern/ActiveObject.h
+++ Editor/Object Selection Strategy Pattern/ActiveObject.h
@@ -9,6 +9,7 @@ class EmptyUI;
 class ScaleUI;
 class GroupUI;
 
+//This manages the overall behavior for selected objects
 class ActiveObject : public SelectedObject
 {
 public:
@@ -17,16 +18,34 @@ public:
 	ActiveObject operator=(const ActiveObject&) = delete;
 	~ActiveObject();
 
+	//Calls the forward movement behavior of the current indicator
 	virtual void ForwardAdjustment() override;
+	
+	//Calls the right movement behavior of the current indicator
 	virtual void RightAdjustment() override;
+	
+	//Calls the left movement behavior of the current indicator
 	virtual void LeftAdjustment()  override;
+	
+	//Calls the backward movement behavior of the current indicator
 	virtual void BackwardAdjustment()  override;
-
+	
+	//Sends world information for the selected object to the selected indicators
 	virtual void ShowSelection() override;
+	
+	//Selects the current indicator group (pos/scale/rot) based on adjustment type
 	virtual void SetAdjustmentType(SelectedObject::adjustment) override;
+	
+	//Switches to a topdown view of the selected object
 	virtual void SnapToObject() override;
+	
+	//Duplicates the selected object (duplicate object can be selected, moved, and is saved)
 	virtual void Duplicate() override;
+	
+	//Deletes this object (won't display during any future runs)
 	virtual void Erase() override;
+	
+	//Calls the indicator group to render its indicators
 	virtual void DrawUI() override;
 
 private:

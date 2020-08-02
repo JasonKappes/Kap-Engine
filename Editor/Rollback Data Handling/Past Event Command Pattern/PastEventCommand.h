153e1ce3d06ef8895fa8e5210102a7d044158d92
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 12:32:08 -0500

    Update PastEventCommand.h
    added comment

--- Editor/Rollback Data Handling/Past Event Command Pattern/PastEventCommand.h
+++ Editor/Rollback Data Handling/Past Event Command Pattern/PastEventCommand.h
@@ -2,6 +2,8 @@
 #define _pasteventcommand
 #include "Matrix.h"
 
+//This is the base class for the rollback command pattern. The rollback handler executes these commands.
+//The behavior changes based on the command executed.
 class PastEventCommand
 {
 public:

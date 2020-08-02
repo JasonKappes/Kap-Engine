5544cccd3e101531a18d9ceb732b122b3c00bd76
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 12:58:55 -0500

    Update EditorInput.h
    added comments

--- Editor/EditorInput.h
+++ Editor/EditorInput.h
@@ -6,6 +6,7 @@
 class SelectedObject;
 class Serializable;
 
+//This object is created during world-edit mode. It manages input and object selection.
 class EditorInput : public Inputable, public Touchable
 {
 public:
@@ -14,22 +15,28 @@ public:
 	EditorInput operator=(const EditorInput&) = delete;
 	~EditorInput();
 
-	void Selection(Selectable*) override;
-
+	//Callback for registered key presses
 	void KeyPressed(AZUL_KEY) override;
 
+	//Callback for registered key holds
 	void KeyHeld(AZUL_KEY) override;
 	
+	//Callbacks for registered mouse action releases
 	void MouseActionReleased(AZUL_MOUSE) override;
 
+	//Callbacks for registered mouse action presses
 	void MouseActionPressed(AZUL_MOUSE) override;
 
+	//Callbacks for registered mouse movements
 	void MouseDirection(AZUL_MOUSE) override;
 
+	//If it has an object selected, it displays any active indicators
 	void VisualizeSelection();
 
+	//If an object is selected, it is sent to this object to be managed.
 	void SetActiveSelection(Serializable*);
-
+	
+	//Switches strategy pattern objects to an "empty object" which isn't affect by any mouse/key input
 	void SetEmptySelection();
 
 private:

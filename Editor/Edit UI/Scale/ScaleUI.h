d5657615f8c67ee8013dbc66e83fd048720f4253
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 11:20:30 -0500

    Update ScaleUI.h
    added comments

--- Editor/Edit UI/Scale/ScaleUI.h
+++ Editor/Edit UI/Scale/ScaleUI.h
@@ -4,6 +4,7 @@
 
 class xyzScaleUI;
 
+//Manages the scale indicators 
 class ScaleUI : public GroupUI
 {
 public:
@@ -12,12 +13,16 @@ public:
 	ScaleUI operator=(const ScaleUI&) = delete;
 	~ScaleUI();
 
+	//This allows the various scale indicators to be selected (registers them for selection)
 	void EnableIndicator() override;
 
+	//This removes the scale indicators from being selected (deregisters them for selection)
 	void ResetIndicator() override;
 
+	//Tells each scale indicator to be rendered
 	void UpdateUI(float radius, const Vect& center) override;
 
+	//Updates scale indicators world information
 	void DrawUI() override;
 
 private:

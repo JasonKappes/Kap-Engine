78fb29c1d292b3dfbc29fb7c6c49161a8d177dca
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 11:07:48 -0500

    Update zRotUI.h
    added comments

--- Editor/Edit UI/Rotation/Rotation Indicators/zRotUI.h
+++ Editor/Edit UI/Rotation/Rotation Indicators/zRotUI.h
@@ -4,6 +4,7 @@
 
 class RotationUI;
 
+//This defines the behavior for the Z rotation indicator
 class zRotUI : public WorldUI
 {
 public:
@@ -14,16 +15,22 @@ public:
 
 	zRotUI(RotationUI*);
 
+	//This renders this indicator at a given position with radius
 	void Display(float radius, const Vect& center) override;
 
+	//This is a callback when this object is selected
 	void Selected() override;
 
+	//This defines the behavior for this object (when selected) if the mouse is moved left
 	void LeftMovement(Serializable*) override;
 
+	//This defines the behavior for this object (when selected) if the mouse is moved right
 	void RightMovement(Serializable*) override;
 
+	//This defines the behavior for this object (when selected) if the mouse is moved forward
 	void ForwardMovement(Serializable*) override;
 
+	//This defines the behavior for this object (when selected) if the mouse is moved backward
 	void BackwardMovement(Serializable*) override;
 
 

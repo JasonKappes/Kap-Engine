f641311895766bd8bbbe66367346c81317240530
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 13:03:50 -0500

    Update WorldEditorManager.h
    added comments

--- Editor/WorldEditorManager.h
+++ Editor/WorldEditorManager.h
@@ -4,6 +4,8 @@
 class EditorInput;
 class CameraManager;
 
+//This manager is only created during world edit mode. 
+//It manages any world-edit resources (just the editor input object)
 class WorldEditorManager
 {
 public:
@@ -13,6 +15,7 @@ public:
 	WorldEditorManager operator=(const WorldEditorManager&) = delete;
 	~WorldEditorManager();
 
+	//Calls the editor input object to display any indicators around a selected object
 	void SendVisualizeRequests() const;
 
 private:

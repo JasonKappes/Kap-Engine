f192bbb4cc767c23e1849eb7642032538c2d797e
Jason Kappes <56867976+JasonKappes@users.noreply.github.com>
2020-08-02 11:00:44 -0500

    Update xRotUI.h
    added comments

--- Editor/Edit UI/Rotation/Rotation Indicators/xRotUI.h
+++ Editor/Edit UI/Rotation/Rotation Indicators/xRotUI.h
@@ -4,6 +4,7 @@
 
 class RotationUI;
 
+//Responsible for the behavior of the X rotation UI indicator
 class xRotUI : public WorldUI
 {
 public:
@@ -14,18 +15,23 @@ public:
 
 	xRotUI(RotationUI*);
 
+	//Renders the ring around a given position with a given radius
 	void Display(float radius, const Vect& center) override;
 
+	//Callback for when this object is selected
 	void Selected() override;
 
+	//This defines the behavior (if selected) upon left movement of the mouse
 	void LeftMovement(Serializable*) override;
 
+	//This defines the behavior (if selected) upon right movement of the mouse	
 	void RightMovement(Serializable*) override;
 
+	//This defines the behavior (if selected) upon forward movement of the mouse
 	void ForwardMovement(Serializable*) override;
 
+	//This defines the behavior (if selected) upon backward movement of the mouse
 	void BackwardMovement(Serializable*) override;
-
 
 private:
 	RotationUI* rotUIcoll;
